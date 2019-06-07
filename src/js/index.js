f8d9d1e6213756d89cd30f1bd95b005a14f9c583
cleamnagata <kindtime02@gmail.com>
2019-06-07 16:23:54 +0900

    update persist message color

--- src/js/index.js
+++ src/js/index.js
@@ -84,6 +84,9 @@ const setUpButtons = persisted => {
     ? 'Storage will not be cleared except by explicit user action. success persist'
     : 'Storage may be cleared by the UA under storage pressure.';
   const div = document.createElement('div');
+  div.style.color = persisted
+    ? '#0500ff'
+    : '#ff0000';
   const text = document.createTextNode(persistMessage);
   div.appendChild(text);
   document.body.appendChild(div);
