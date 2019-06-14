377dc9541a256742f71e5f80484aff96af0e3fab
cleamnagata <kindtime02@gmail.com>
2019-06-14 14:19:39 +0900

    change dummy file

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ import fillArray from './util/fillArray';
 
 const config = {
   RESOURCE: '/assets/1000000byte.txt',
-  DUMMY_RESOURCE_PREFIX: '/assets/dummy-1000000byte-',
+  DUMMY_RESOURCE_PREFIX: '/assets/dummy-100byte-',
   VERSIONS: fillArray(100), // 最大数
   LOAD_QUE: 10, // 同時にRequest投げる上限
   MAX_SHARD: 1,
